2837a23245297dfa4aee07fd57a132a0a98299cc
jorgegallegos <jgallegosek@hotmail.com>
2019-09-07 20:45:25 -0500

    Fill Entity Propery

--- WCFServicesApp/Persistence/Data.cs
+++ WCFServicesApp/Persistence/Data.cs
@@ -19,8 +19,8 @@ namespace WCFServicesApp.Persistence
         {
             return new List<Domain.Developer>
             {
-                new Domain.Developer {DeveloperID = 1,FullName ="John Luiton"},
-                new Domain.Developer {DeveloperID = 2,FullName ="Mary Well"},
+                new Domain.Developer {DeveloperID = 1,FullName ="John Luiton", Position = ".NET Developer"},
+                new Domain.Developer {DeveloperID = 2,FullName ="Mary Well", Position = "Java Developer"},
                 new Domain.Developer {DeveloperID = 3,FullName ="Rick Levis"},
                 new Domain.Developer {DeveloperID = 4,FullName ="Coleman Regal"},
                 new Domain.Developer {DeveloperID = 5,FullName ="Mark Ton"},
